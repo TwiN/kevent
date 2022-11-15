3103f697d01abe8866db534feb1e59734eafbcca
TwiN <twin@linux.com>
2022-11-14 20:07:14 -0500

    feat: Allow specifying resource kind

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # kevent
-
 Very simple library for creating Kubernetes events.
 
 ```go
+kevent.CreateEvent(kubernetesClient, resourceNamespace, resourceKind, resourceName, reason, message, isWarning)
+```
+Where:
+- `kubernetesClient` is a Kubernetes client (e.g. `kubernetes.Clientset`). Note that for testing purposes, the actual parameter type is kubernetes.Interface, which `kubernetes.Clientset` implements.
+- `resourceNamespace` is the namespace of the resource that the event is related to.
+- `resourceKind` is the kind of the resource that the event is related to.
+- `resourceName` is the name of the resource that the event is related to.
+- `reason` is the reason for the event (e.g. `FailedToCreate`, `Scheduled`, `FailedToSchedule`, etc.).
+- `message` is the message for the event.
+- `isWarning` is a boolean indicating whether the event is of type `Warning` or `Normal`.
+
+Example:
+```go
 clientConfig, _ := rest.InClusterConfig()
 kubernetesClient, _ := kubernetes.NewForConfig(clientConfig)
-kevent.CreateEvent(kubernetesClient, "kube-system", "pod-name", "SomeReason", "Things went boom", true)
+kevent.CreateEvent(kubernetesClient, "kube-system", "pod", "nginx", "Restarted", "Application was unstable", true)
 ```
--- kevent.go
+++ kevent.go
@@ -2,7 +2,7 @@ package kevent
 
 import (
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	typedv1core "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -16,12 +16,8 @@ var (
 	recorder    record.EventRecorder    = nil
 )
 
-func init() {
-	_ = corev1.AddToScheme(scheme)
-}
-
 // CreateEvent creates a Kubernetes Event with the given parameters using the given kubernetes Client.
-func CreateEvent(kubernetesClient kubernetes.Interface, namespace, podName, reason, message string, isWarning bool) {
+func CreateEvent(kubernetesClient kubernetes.Interface, resourceNamespace, resourceKind, resourceName, reason, message string, isWarning bool) {
 	if broadcaster == nil {
 		broadcaster = record.NewBroadcaster()
 		broadcaster.StartStructuredLogging(4)
@@ -34,10 +30,14 @@ func CreateEvent(kubernetesClient kubernetes.Interface, namespace, podName, reas
 	} else {
 		eventType = corev1.EventTypeNormal
 	}
-	recorder.Event(&corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      podName,
-			Namespace: namespace,
+	us := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"kind": resourceKind,
+			"metadata": map[string]interface{}{
+				"name":      resourceName,
+				"namespace": resourceNamespace,
+			},
 		},
-	}, eventType, reason, message)
+	}
+	recorder.Event(us, eventType, reason, message)
 }
